516f8bc12b98a86fb236f18494b7da5f3af9cc83
Сергей Николаевич <tukmachev.18@yandex.ru>
2023-06-01 16:22:20 +0300

    Первый коммит

---
+++ src/test/java/com/example/coursework3_3/CourseWork33ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework3_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork33ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
